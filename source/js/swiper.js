2c2b156a137e17ffee0483989ac46ba73e027597
Grony <gr0ny.0017@gmail.com>
2023-02-08 21:25:23 +0300

    Готовая мобильная версия слайдера

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,26 +1,17 @@
 export const swiperTopProducts= new Swiper('.swiper', {
-
   loop: true,
-  // If we need pagination
-  // pagination: {
-  //   el: '.swiper-pagination',
-  // },
+  pagination: {
+    el: '.swiper__pagination',
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper__button--next',
     prevEl: '.swiper__button--prev',
   },
-
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-  speed: 700,
+  speed: 500,
   // effect: 'fade',
   // fadeEffect: {
   //   crossFade: true,
   // }
-
-
 });
